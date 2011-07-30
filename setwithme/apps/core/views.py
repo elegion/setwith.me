d44321830f8d9218c5b391c0bc00b15e2205cadf
Igor Kobylin <igor.kobylin@e-legion.com>
2011-07-30 12:02:29 +0400

    [R] Searching for a new game moved into a separate view

--- setwithme/apps/core/views.py
+++ setwithme/apps/core/views.py
@@ -5,3 +5,10 @@ from annoying.decorators import render_to
 @render_to('core/index.html')
 def home(request):
     return {}
+
+
+@render_to('core/lobby.html')
+def lobby(request):
+    return {
+        'message': 'Set is designed by Marsha Falco in 1974 and published by Set Enterprises in 1991'
+    }
