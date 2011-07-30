9a7e48b9104fa4d7c47e5300a1d2b0894184d982
Serge Travin <serge.travin@e-legion.com>
2011-07-30 04:49:06 +0400

    [+] stub root view

---
+++ setwithme/apps/core/views.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+from django.shortcuts import render_to_response
+
+def home(request):
+    return render_to_response("core/index.html", {})
--- setwithme/urls.py
+++ setwithme/urls.py
@@ -6,7 +6,7 @@ from django.conf.urls.defaults import patterns, include, url
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'setwithme.views.home', name='home'),
+    url(r'^$', 'core.views.home', name='home'),
     # url(r'^setwithme/', include('setwithme.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
