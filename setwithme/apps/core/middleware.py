5c9e87afd5fc9aa6c6a5f613130e3bed582bd727
Serge Travin <serge.travin@e-legion.com>
2011-07-30 19:31:17 +0400

    [*] Do not override fb user by anon user

--- setwithme/apps/core/middleware.py
+++ setwithme/apps/core/middleware.py
@@ -6,6 +6,8 @@ from django.contrib import auth
 class CookieAuthMiddleware(object):
 
     def process_request(self, request):
+        if not request.user.is_anonymous():
+            return None
         user_uuid = request.session.get('anon_uuid', '')
         user = auth.authenticate(
             user_uuid=user_uuid)
