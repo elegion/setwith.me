ffefedf82d879780f8ae10ed70173cc05c00b262
Serge Travin <serge.travin@e-legion.com>
2011-07-30 20:53:56 +0400

    [+] Add middleware for csrf token cookie

--- setwithme/apps/core/middleware.py
+++ setwithme/apps/core/middleware.py
@@ -16,3 +16,9 @@ class CookieAuthMiddleware(object):
             user.last_login = datetime.datetime.now()
             user.save()
             request.user = user
+
+
+class CsrfFixMiddleware(object):
+    def process_view(self, request, view_func, callback_args, callback_kwargs):
+        request.META["CSRF_COOKIE_USED"] = True
+        return None
--- setwithme/settings.py
+++ setwithme/settings.py
@@ -108,6 +108,7 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
+    'core.middleware.CsrfFixMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'core.facebook_sdk.SetWithMeFacebookMiddleware',
