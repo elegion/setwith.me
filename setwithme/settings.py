0e63db36052cc87698c8b4f1a16ecbf7455e4607
Serge Travin <serge.travin@e-legion.com>
2011-07-30 05:17:20 +0400

    [+] Add South

--- setwithme/settings.py
+++ setwithme/settings.py
@@ -132,6 +132,10 @@ INSTALLED_APPS = (
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
 
+    # Third-party apps
+    'south',
+
+    # Our apps
     'core',
     'users',
 )
