d8baf525832818dab6a465c5f1a273cb8c9b754f
Serge Travin <serge.travin@e-legion.com>
2011-08-01 02:32:13 +0400

    [*] Database cache backend

--- setwithme/settings.py
+++ setwithme/settings.py
@@ -205,13 +205,15 @@ LOGIN_URL = '/'
 
 DEFAULT_PROFILE_PIC = os.path.join(STATIC_URL, '/static/images/nophoto.png')
 
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.db.DatabaseCache',
+        'LOCATION': 'cache_table',
+    }
+}
+
 try:
     from settings_local import *
 except ImportError:
     pass
 
-CACHES = {
-    'default': {
-        'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
-    }
-}
